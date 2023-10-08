ba6b27be6b685d94295a894837a6cc3515e2dd19
Mika Akizuki <25174488+shanoaice@users.noreply.github.com>
2023-10-08 20:34:20 +0800

    feature(constants): auth constants

---
+++ src/constants.h
@@ -0,0 +1,21 @@
+#include <string>
+
+typedef std::string string;
+
+// Microsoft Entra Client / Application ID
+const string APP_CLIENT_ID = "50855311-bbcd-4690-9fef-9101ee992964";
+
+const string GLOBAL_AUTH_ENDPOINT = "https://login.microsoftonline.com";
+const string GLOBAL_GRAPH_ENDPOINT = "https://graph.microsoftonline.com";
+
+const string US_L4_AUTH_ENDPOINT = "https://login.microsoftonline.us";
+const string US_L4_GRAPH_ENDPOINT = "https://graph.microsoft.us";
+
+const string US_L5_AUTH_ENDPOINT = "https://login.microsoftonline.us";
+const string US_L5_GRAPH_ENDPOINT = "https://dod-graph.microsoft.us";
+
+const string DE_AUTH_ENDPOINT = "https://login.microsoftonline.de";
+const string DE_GRAPH_ENDPOINT = "https://graph.microsoft.de";
+
+const string CN_AUTH_ENDPOINT = "https://login.chinacloudapi.cn";
+const string CN_GRAPH_ENDPOINT = "https://microsoftgraph.chinacloudapi.cn";
